4623508e7b969b780fc9ac26038684707c5554a5
panjri <705313@pdsb.net>
2023-05-26 09:34:01 -0400

    Initial Commit

---
+++ HackHive App (Final)/HackHive_App__Final_App.swift
@@ -0,0 +1,17 @@
+//
+//  HackHive_App__Final_App.swift
+//  HackHive App (Final)
+//
+//  Created by Student on 2023-05-26.
+//
+
+import SwiftUI
+
+@main
+struct HackHive_App__Final_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
